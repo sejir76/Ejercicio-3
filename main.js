2f404bbed433e093a33e6092093e3ff175e871a0
rafa <homer512x@gmail.com>
2022-11-11 16:42:48 +0100

    Añadir archivo js

---
+++ main.js
@@ -0,0 +1,19 @@
+//Crea dos variables, una llamada numero1 y otra llamada numero2
+
+var numero1 = 10
+var numero2 = 5
+
+suma = numero1 + numero2
+
+console.log(suma)
+
+//Crea otras variables de tipo number y realiza las operaciones de resta, multiplicación y división.
+
+let number1 = 20
+let number2 = 10
+
+resta = console.log(number1 - number2)
+
+multiplicacion = console.log(number2 * number1)
+
+division = console.log(number1 / number2)
